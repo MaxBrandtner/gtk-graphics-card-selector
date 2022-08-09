9a169ad71ed8b479061ab0d903fbf5fdbfece378
MaxBrandtner <110912574+MaxBrandtner@users.noreply.github.com>
2022-08-09 20:30:31 +0700

    Create graphics-chooser.py

---
+++ graphics-chooser.py
@@ -0,0 +1,34 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from os import system
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="switch graphics mode")
+        self.set_border_width(10)
+        self.set_default_size(500, 200)
+
+        self.box = Gtk.Box(spacing = 10)
+        self.add(self.box)
+
+        self.button_integrated = Gtk.Button()
+        self.button_integrated.set_label("use integrated graphics(battery)")
+        self.button_integrated.connect("clicked", self.integrated_clicked)
+        self.box.pack_start(self.button_integrated, True, True, 0)
+
+        self.button_dedicated = Gtk.Button()
+        self.button_dedicated.set_label("use dedicated graphics(performance)")
+        self.button_dedicated.connect("clicked", self.dedicated_clicked)
+        self.box.pack_start(self.button_dedicated, True, True, 0)
+
+    def integrated_clicked(self, widget):
+        system("integrated-graphics")
+
+    def dedicated_clicked(self, widget):
+        system("dedicated-graphics")
+
+window = MainWindow()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+Gtk.main()
